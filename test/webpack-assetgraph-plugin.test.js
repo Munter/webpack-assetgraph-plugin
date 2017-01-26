11ed8f7923c5c4fd43a7724c09a9cee0061a78e3
Peter Müller <munter@fumle.dk>
2017-01-26 23:20:24 +0100

    Proof of concept of injecting a new file into a webpack compilation

--- test/webpack-assetgraph-plugin.test.js
+++ test/webpack-assetgraph-plugin.test.js
@@ -1,7 +1,7 @@
 var expect = require('unexpected').clone();
 
 var webpack = require('webpack');
-var plugin = require('../lib');
+// var plugin = require('../lib');
 var config = require('../webpack.config.test');
 
 function testWebpackWithConfig (config, expectedFiles) {
@@ -33,6 +33,7 @@ function testWebpackWithConfig (config, expectedFiles) {
 describe('webpack-assetgraph-plugin', function () {
   it('should not fail', function () {
     return testWebpackWithConfig(config, [
-                                 ]);
+      'output.js'
+    ]);
   });
 });
